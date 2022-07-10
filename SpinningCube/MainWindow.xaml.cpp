b3fc85f45eb9b12ba6275303669858b76dddd6e4
Emmett Tsai <emmett.tsai@gmail.com>
2022-07-10 22:37:00 +0800

    Move DirectX 11 and XAML App template project to WinUI3
    Template project:
    DirectX 11 and XAML App (Universal Windows - C++/CX)
    
    Reference:
    https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/move-to-winrt-from-cx
    https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/consume-com
    https://docs.microsoft.com/en-us/uwp/cpp-ref-for-winrt/com-ptr
    https://github.com/microsoft/cppwinrt/issues/97
    https://github.com/microsoft/Windows-universal-samples/blob/main/Samples/BasicHologram/cppwinrt/Content/SpinningCubeRenderer.cpp
    https://github.com/microsoft/microsoft-ui-xaml/issues/3854
    https://github.com/kennykerr/cppwinrt/blob/master/Store/Direct2D/App.cpp
    https://kennykerr.ca/2018/03/13/cppwinrt-coroutines-thread-pool/
    https://stackoverflow.com/questions/50788733/c-winrts-com-ptr-reset
    https://stackoverflow.com/questions/71507022/how-to-implement-threadpoolrunasync-in-winrt-c

--- SpinningCube/MainWindow.xaml.cpp
+++ SpinningCube/MainWindow.xaml.cpp
@@ -4,31 +4,30 @@
 #include "MainWindow.g.cpp"
 #endif
 
+#include "DirectXPage.g.h"
+
 using namespace winrt;
-using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Media::Animation;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace winrt::SpinningCube::implementation
 {
+    // https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/consume-com
+    // https://docs.microsoft.com/en-us/uwp/cpp-ref-for-winrt/com-ptr
+    // https://stackoverflow.com/questions/50788733/c-winrts-com-ptr-reset
+    // https://github.com/kennykerr/cppwinrt/blob/master/Store/Direct2D/App.cpp
+    // https://stackoverflow.com/questions/71507022/how-to-implement-threadpoolrunasync-in-winrt-c
+    // https://kennykerr.ca/2018/03/13/cppwinrt-coroutines-thread-pool/
+    // https://github.com/microsoft/cppwinrt/issues/97
+    // https://github.com/microsoft/Windows-universal-samples/blob/main/Samples/BasicHologram/cppwinrt/Content/SpinningCubeRenderer.cpp
+    // https://github.com/microsoft/microsoft-ui-xaml/issues/3854
+
     MainWindow::MainWindow()
     {
         InitializeComponent();
-    }
 
-    int32_t MainWindow::MyProperty()
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::MyProperty(int32_t /* value */)
-    {
-        throw hresult_not_implemented();
-    }
-
-    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
-    {
-        myButton().Content(box_value(L"Clicked"));
+        RootFrame().Navigate(xaml_typename<winrt::SpinningCube::DirectXPage>(), nullptr, SuppressNavigationTransitionInfo());
     }
 }
