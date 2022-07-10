71e8c6b982583fb7852b5f8662dc8569a047b874
Emmett Tsai <emmett.tsai@gmail.com>
2022-07-10 22:42:17 +0800

    Update NutGet

--- SpinningCube/MainWindow.xaml.cpp
+++ SpinningCube/MainWindow.xaml.cpp
@@ -14,16 +14,6 @@ using namespace Microsoft::UI::Xaml::Media::Animation;
 
 namespace winrt::SpinningCube::implementation
 {
-    // https://docs.microsoft.com/en-us/windows/uwp/cpp-and-winrt-apis/consume-com
-    // https://docs.microsoft.com/en-us/uwp/cpp-ref-for-winrt/com-ptr
-    // https://stackoverflow.com/questions/50788733/c-winrts-com-ptr-reset
-    // https://github.com/kennykerr/cppwinrt/blob/master/Store/Direct2D/App.cpp
-    // https://stackoverflow.com/questions/71507022/how-to-implement-threadpoolrunasync-in-winrt-c
-    // https://kennykerr.ca/2018/03/13/cppwinrt-coroutines-thread-pool/
-    // https://github.com/microsoft/cppwinrt/issues/97
-    // https://github.com/microsoft/Windows-universal-samples/blob/main/Samples/BasicHologram/cppwinrt/Content/SpinningCubeRenderer.cpp
-    // https://github.com/microsoft/microsoft-ui-xaml/issues/3854
-
     MainWindow::MainWindow()
     {
         InitializeComponent();
